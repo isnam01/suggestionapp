59438eec85de59689ab8889cd568374633f817cc
isnam01 <guptamansi199901@gmail.com>
2020-12-08 23:18:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import soundfile from './Christina Perri - A Thousand Years.mp3';
 
-function App() {
+export default function App() {
+  const dict = {
+    "Instrumental 🎶": [{song:"Faded 😍💞",artist:"Armaan Malik",link:soundfile},{song:"Ye Haseen Vadiyan 🤩💖",artist:"Armaan Malik",link:soundfile},{song:"Bahon ke Darmiyan 🤩💕",artist:"Armaan Malik",link:soundfile}],
+    "HipHop 🎧": [{song:"buffalo says hello ❤",artist:"Armaan Malik",link:soundfile},{song:"hello bllo",artist:"Armaan Malik",link:soundfile}],
+    "EDM 🎛": [{song:"hello aditi 😍",artist:"Armaan Malik",link:soundfile},{song:"hello bel 🥰",artist:"Armaan Malik",link:soundfile}],
+    "Folk Music ✨": [{song:"Kesariya Balam 💘💘",link:"song"},{song:"Chaudhary 💞💞",artist:"Armaan Malik",link:soundfile}],
+    "Jazz 🎵": [{song:"hello adi 😍",artist:"Armaan Malik",link:soundfile},{song:"hello bello",artist:"Armaan Malik",link:soundfile}]
+  };
+  const [meaning, setmeaning] = useState(dict["aditi"]);
+  const keys = Object.keys(dict);
+  function handleclick(key) {
+    setmeaning(dict[key]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contents">
+        <h2>Music Suggestion App for Music Lovers 😍😍😍</h2>
+        <h2>🎧+🎶=💖</h2>
+      <h4>
+        {keys.map((key) => {
+          return (
+            <span className="types" key={key} onClick={() => handleclick(key)}>
+              {key}
+            </span>
+          );
+        })}
+      </h4>
+      <h3 className="songlist">
+        {meaning
+          ? meaning.map((val) => {
+              return (
+                <div className="song">
+                  <span style={{"padding":"15px"}} key={val.song}>{val.song}</span>
+                  <p>{val.artist}</p>
+                  <audio controls  style={{"outline":"none"}}>
+                    <source src={val.link} type="audio/ogg" />
+                      Your browser does not support the audio element.
+                  </audio>
+              </div>
+              )
+            })
+          : null}
+      </h3>
+    </div>
     </div>
   );
 }
-
-export default App;
